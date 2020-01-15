20ae304a4243523f7e8fc3ac544fd162d1e794ed
dorswisa <swisalem@gmail.com>
2020-01-16 01:41:10 +0200

    update home.js

--- icb/app/public/js/views/home.js
+++ icb/app/public/js/views/home.js
@@ -1,9 +1,23 @@
 
 $(document).ready(function(){
-
 	var hc = new HomeController();
 	var av = new AccountValidator();
-	
+
+	var touch 	= $('#resp-menu');
+	var menu 	= $('.menu');
+
+	$(touch).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+
+	$(window).resize(function(){
+		var w = $(window).width();
+		if(w > 767 && menu.is(':hidden')) {
+			menu.removeAttr('style');
+		}
+	});
+
 	$('#account-form').ajaxForm({
 		beforeSubmit : function(formData, jqForm, options){
 			if (av.validateForm() == false){
@@ -28,7 +42,7 @@ $(document).ready(function(){
 	$('#name-tf').focus();
 
 // customize the account settings form //
-	
+
 	$('#account-form h2').text('Account Settings');
 	$('#account-form #sub').text('Here are the current settings for your account.');
 	$('#user-tf').attr('disabled', 'disabled');
@@ -47,4 +61,56 @@ $(document).ready(function(){
 	$('.modal-confirm .submit').html('Delete');
 	$('.modal-confirm .submit').addClass('btn-danger');
 
+
+// setup the confirm window that displays when the user chooses to delete their tiles //
+	$('.modal-delete-tile').modal({ show : false, keyboard : true, backdrop : true });
+	$('.modal-delete-tile .modal-header h4').text('Delete Tile');
+	$('.modal-delete-tile .modal-body p').html('Are you sure you want to delete this tiles?');
+	$('.modal-delete-tile .cancel').html('Cancel');
+	$('.modal-delete-tile .submit').html('Delete');
+	$('.modal-delete-tile .submit').addClass('btn-danger');
+
+// setup the confirm window that displays when the user chooses to logout //
+
+	$('.modal-logout').modal({ show : false, keyboard : true, backdrop : true });
+	$('.modal-logout .modal-header h4').text('Sign Out');
+	$('.modal-logout .modal-body p').html('Are you sure you want to logout?');
+	$('.modal-logout .cancel').html('No');
+	$('.modal-logout .submit').html('Yes');
+	$('.modal-logout .submit').addClass('btn-danger');
+
 });
+
+function myTnTool( action, item ) {										// custom button switch
+	console.dir(item);
+	switch( action ) {
+		case 'custom1':
+			SoundCustom(item);
+			break;
+	}
+}
+
+async function SoundCustom(item){
+	var text = item.title;
+	if('speechSynthesis' in window) { 											// Chrome only
+		var speech = new SpeechSynthesisUtterance( text );
+		speech.lang = 'en-US';
+		window.speechSynthesis.speak(speech);
+	}
+	else
+	{
+		text = encodeURIComponent(text);
+		var audio = new Audio('https://translate.google.com/translate_tts?ie=UTF-8&client=tw-ob&tl=en&q='+text);
+		audio.type = 'audio/mpeg';
+
+		try {
+			await audio.play();
+			console.log('Playing...');
+		} catch (err) {
+			console.log('Failed to play...' + error);
+		}
+	}
+}
+
+
+
