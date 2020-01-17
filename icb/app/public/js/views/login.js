81cec6bbcc64702a10753c6a38f12242370e6a1a
abutbab1 <58395486+abutbab1@users.noreply.github.com>
2020-01-17 13:17:08 +0200

    login and signup scripts

---
+++ icb/app/public/js/views/login.js
@@ -0,0 +1,38 @@
+
+$(document).ready(function(){
+
+	var lv = new LoginValidator();
+	var lc = new LoginController();
+
+// main login form //
+
+	$('#login').ajaxForm({
+		beforeSubmit : function(formData, jqForm, options){
+			if (lv.validateForm() == false){
+				return false;
+			} 	else{
+			// append 'remember-me' option to formData to write local cookie //
+				formData.push({name:'remember-me', value:$('#btn_remember').find('span').hasClass('fa-check-square')});
+				return true;
+			}
+		},
+		success	: function(responseText, status, xhr, $form){
+			if (status == 'success') window.location.href = '/home';
+		},
+		error : function(e){
+			lv.showLoginError('Login Failure', 'Please check your username and/or password');
+		}
+	}); 
+
+	$("input:text:visible:first").focus();
+	$('#btn_remember').click(function(){
+		var span = $(this).find('span');
+		if (span.hasClass('fa-minus-square')){
+			span.removeClass('fa-minus-square');
+			span.addClass('fa-check-square');
+		}	else{
+			span.addClass('fa-minus-square');
+			span.removeClass('fa-check-square');
+		}
+	});
+});
