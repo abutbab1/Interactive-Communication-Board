b2e0d295a5fe412217f0b79a10082db546100f54
abutbab1 <58395486+abutbab1@users.noreply.github.com>
2020-01-17 13:35:29 +0200

    remmeber me button

---
+++ icb/app/public/js/controllers/loginController.js
@@ -0,0 +1,19 @@
+
+function LoginController()
+{
+// bind event listeners to button clicks //
+	$('#login .button-rememember-me').click(function(e) {
+		var span = $(this).find('span');
+		if (span.hasClass('glyphicon-unchecked')){
+			span.addClass('glyphicon-ok');
+			span.removeClass('glyphicon-unchecked');
+		}	else{
+			span.removeClass('glyphicon-ok');
+			span.addClass('glyphicon-unchecked');
+		}
+	});
+
+// automatically toggle focus between the email modal window and the login form //
+	$('#get-credentials').on('shown.bs.modal', function(){ $('#email-tf').focus(); });
+	$('#get-credentials').on('hidden.bs.modal', function(){ $('#user-tf').focus(); });
+}
